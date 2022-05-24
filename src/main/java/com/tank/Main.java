ddf049a308f3a43295bb65aac2a75f7507a1e871
1193131227@qq.com <1193131227@qq.com>
2022-05-24 23:47:25 +0800

    设置git提交文件

---
+++ src/main/java/com/tank/Main.java
@@ -0,0 +1,14 @@
+package com.tank;
+
+/**
+ * Copyright (C), 2017-2022, 赵旭
+ * Author: 11931
+ * Date: 2022/5/24 23:32
+ * FileName: Main
+ * Description: com.tank
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
